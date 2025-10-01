e40c81ae4a4db52be4e484774eb637b493c22b59
Your Name <you@example.com>
2025-10-01 12:36:08 +0200

    fix: bugfixes around navbar showing content missing

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.18
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.18
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.300
