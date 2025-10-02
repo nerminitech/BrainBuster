eeca69f22f33f17e8e3d64f4557cc3948ccc83b7
Your Name <you@example.com>
2025-10-02 11:25:52 +0200

    change: removed hints from view

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module BrainBuster
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.i18n.available_locales = %i[de en]
+    config.i18n.default_locale = :de
+    config.i18n.fallbacks = [:de, :en]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
