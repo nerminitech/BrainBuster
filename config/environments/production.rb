31da24ee94b0006419b1df91f7c2548bdcf90d0e
Your Name <you@example.com>
2025-10-06 10:11:52 +0200

    remove: kategorien link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  assume_ssl_enabled = !ENV.fetch("RAILS_ASSUME_SSL", "true").casecmp?("false")
+  config.assume_ssl = assume_ssl_enabled
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = !ENV.fetch("RAILS_FORCE_SSL", "true").casecmp?("false")
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
